22fb74c5b487b89b62901ed925bd685367f27d3e
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2023-09-18 15:24:30 -0600

    week01 homework - no js

---
+++ week01/js/date.js
@@ -0,0 +1,3 @@
+
+const options = {weekdate: "long", day: "numeric", month: "long", year: "numeric"};
+document.getElementById("current-date").textContent = new Date().toLocaleDateString("en-US", options);
