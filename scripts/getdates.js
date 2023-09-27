4bf01253ad13c15180ad9d58926a7228cc8ceccb
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2023-09-26 18:40:38 -0600

    HAMBURGUER MENU, BUGGED

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,15 @@ const options = {
     year: "numeric"
 };
 document.querySelector("#yearDate").innerHTML =  new Date().toLocaleDateString("en-US", options);
+
+
+
+//HAMBURGUER BUTTON//
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
