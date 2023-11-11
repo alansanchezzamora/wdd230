a806a1d7ec6ac35f9d8273547ab981d29e337f6a
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2023-11-10 22:25:49 -0600

    w6

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,26 @@
+let oLastModif = new Date(document.lastModified);
+document.querySelector("#lastModified").textContent = oLastModif;
+
+const options = {
+    year: "numeric"
+};
+document.querySelector("#yearDate").innerHTML =  new Date().toLocaleDateString("en-US", options);
+
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
+
