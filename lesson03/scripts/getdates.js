44460c3e0e1f8695d9952e262ae8586e6e8508b6
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2023-11-23 20:57:47 -0600

    chamber 2 - BOM local storage acts

---
+++ lesson03/scripts/getdates.js
@@ -0,0 +1,10 @@
+let oLastModif = new Date(document.lastModified);
+document.querySelector("#lastModified").textContent = oLastModif;
+
+const options = {
+  year: "numeric",
+};
+document.querySelector("#yearDate").innerHTML = new Date().toLocaleDateString(
+  "en-US",
+  options
+);
